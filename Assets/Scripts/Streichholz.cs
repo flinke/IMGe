eb6786fbfd941829444bd1c3728eaf4383f8de9e
friedliche <irina.tsareva@hotmail.com>
2017-01-14 00:39:47 +0100

    ok

---
+++ Assets/Scripts/Streichholz.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Streichholz : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    //controller vibiriert und neues spiel fängt an:
+    // das streichholz fliegt hin
+
+    //Methode laesst streichholzschachtel hinfliegen
+
+
+    //bewegung des streicholz mithilfe des controllers
+
+    //dabei auch eine rauchschwade erscheinen lassen
+
+    //wenn schnell genug, dann feuer
+
+    //streichholz wird auch wand geworfen und wand geht in feuer auf 
+    //wenn wand collider von streichholz berührt, dann wird das particle system aktiviert
+
+    //schachtel fliegt wieder zurück
+
+}
