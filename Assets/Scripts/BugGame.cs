e7dabdf24353ff5181fc6b61fb57bf6811d8cd66
Mini <m.luong@hotmail.de>
2017-01-18 19:04:44 +0100

    bugs tbc ............ :crying_cat_face:

--- Assets/Scripts/BugGame.cs
+++ Assets/Scripts/BugGame.cs
@@ -3,36 +3,55 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-public class BugGame : MonoBehaviour {
+public class BugGame : MonoBehaviour
+{
     public GameObject ParentBee;
     private Transform[] _beeArray;
     private Dictionary<int, bool> _isBeeUp;
+    private float _beeHeight = 0f;
+    private float _beeLow = 0f;
 
 
     // Use this for initialization
-    void Start() {
+    void Start()
+    {
         SetBumbleBeeArray(ParentBee);
         SpawnBee();
     }
 
     // Update is called once per frame
-    void Update() {
-        for (int i = 0; i < _beeArray.Length; i++) {
-            Debug.Log(i + " " + _isBeeUp[i] + " " + _beeArray[i].transform.position.y);
-            if (_isBeeUp[i] && _beeArray[i].transform.position.y < 0.47f) {
+    void Update()
+    {
+        for (int i = 0; i < _beeArray.Length; i++)
+        {
+            Debug.Log(_isBeeUp[i] + " " + _beeArray[i].transform.position.y);
+            if (_isBeeUp[i] && _beeArray[i].transform.position.y < 0.47f)
+            {
+                Debug.Log(i + " " + _isBeeUp[i] + " " + _beeArray[i].transform.position.y);
+                _beeHeight += Time.deltaTime;
                 _beeArray[i].transform.position = Vector3.Lerp(
                     new Vector3(_beeArray[i].transform.position.x, 0.2f, _beeArray[i].transform.position.z),
-                    new Vector3(_beeArray[i].transform.position.x, 0.47f, _beeArray[i].transform.position.z), 10000 / Time.deltaTime);
+                    new Vector3(_beeArray[i].transform.position.x, 0.47f, _beeArray[i].transform.position.z), _beeHeight);
             }
+            else if (!_isBeeUp[i] && _beeArray[i].transform.position.y > 0.2f)
+            {
+                _beeLow += Time.deltaTime;
+                _beeArray[i].transform.position = Vector3.Lerp(
+                    new Vector3(_beeArray[i].transform.position.x, 0.47f, _beeArray[i].transform.position.z),
+                    new Vector3(_beeArray[i].transform.position.x, 0.2f, _beeArray[i].transform.position.z), _beeLow);
+            }
+
         }
     }
 
     // add transform components of children to array
-    void SetBumbleBeeArray(GameObject parentBee) {
+    void SetBumbleBeeArray(GameObject parentBee)
+    {
         int beeCount = parentBee.transform.childCount;
         _beeArray = new Transform[beeCount];
         _isBeeUp = new Dictionary<int, bool>(beeCount);
-        for (int i = 0; i < beeCount; i++) {
+        for (int i = 0; i < beeCount; i++)
+        {
             _beeArray[i] = parentBee.transform.GetChild(i);
             _isBeeUp[i] = false;
         }
@@ -40,18 +59,24 @@ public class BugGame : MonoBehaviour {
     }
 
     // spawn random bee
-    void SpawnBee() {
+    void SpawnBee()
+    {
         int beeNum;
-        do {
+        _beeHeight = 0f;
+        do
+        {
             beeNum = Random.Range(0, _beeArray.Length - 1);
             _isBeeUp[beeNum] = true;
             StartCoroutine(DespawnBee(beeNum));
         } while (!_isBeeUp[beeNum]);
+        StartCoroutine(DespawnBee(beeNum));
     }
 
     // bling bling and reset bee
-    IEnumerator BlinkBee(int beeNum) {
-        for (int i = 0; i < 10; i++) {
+    IEnumerator BlinkBee(int beeNum)
+    {
+        for (int i = 0; i < 10; i++)
+        {
             _beeArray[beeNum].GetComponent<Renderer>().enabled
                 = !_beeArray[beeNum].GetComponent<Renderer>().enabled;
             yield return new WaitForSeconds(0.05f);
@@ -61,19 +86,31 @@ public class BugGame : MonoBehaviour {
     }
 
     // despawn bee
-    IEnumerator DespawnBee(int beeNum) {
+    IEnumerator DespawnBee(int beeNum)
+    {
         yield return new WaitForSeconds(3f);
-        if (_isBeeUp[beeNum]) {
-            _beeArray[beeNum].transform.position = Vector3.Lerp(
-    new Vector3(_beeArray[beeNum].transform.position.x, 0.2f, _beeArray[beeNum].transform.position.z),
-    new Vector3(_beeArray[beeNum].transform.position.x, 0.47f, _beeArray[beeNum].transform.position.z), 10000 / Time.deltaTime);
+
+        if (_isBeeUp[beeNum])
+        {
             _isBeeUp[beeNum] = false;
+
         }
     }
 
     // kill bee if hit
-
-
     // lower HP if hit false button
+    void killBee(int pressedButton)
+    {
+        if (_isBeeUp[pressedButton])
+        {
+            StartCoroutine(BlinkBee(pressedButton));
+
+        }
+        else
+        {
+            //HP--
+        }
+    }
+
 
 }
