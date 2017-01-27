25eb391f71894e2792dc23edd1ad1717b3e65dde
friedliche <irina.tsareva@hotmail.com>
2017-01-27 10:55:54 +0100

    intensity vom potspotlight steigt im update

---
+++ Assets/Scripts/PotGame.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PotGame : MonoBehaviour {
+
+    private bool _gameStarted = true;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (_gameStarted)
+        {
+            transform.GetChild(0).GetComponent<Light>().intensity += 0.01f;
+        }
+
+	}
+
+    public void StartPotGame()
+    {
+        _gameStarted = true;
+    }
+}
